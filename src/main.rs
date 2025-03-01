dfba06c14ae372fcbc0a1ac57c7045d84a386bef
Egor <svetlega10@gmail.com>
2025-03-01 14:00:57 +0300

    Добавлен метод create_file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use core::error;
+use std::{ascii::AsciiExt, collections::HashMap};
+
+enum Errors {
+    InvalidPathe(String)
+}
+#[derive(Debug)]
+enum Entry {
+    File(String),
+    Directory(HashMap<String, Entry>)
+}
+
+struct FileSystem {
+    root: Entry,
+}
+
+impl FileSystem {
+    fn new() -> Self {
+        FileSystem {
+            root: Entry::Directory(HashMap::new()),
+        }
+    }
+
+    fn create_file(&mut self, path: &str, content: &str) -> Result<(), String> {
+        let parts: Vec<&str> = path.split('/').filter(|&part| !part.is_empty()).collect();
+
+        if parts.is_empty() {
+            return Err("Invalid path".to_string())
+        };
+
+        let file_name = parts.last().unwrap().to_string();
+        let dir_path = parts[..parts.len() - 1].join("/");
+
+        let dir = self.get_directory_mut(&dir_path)?;
+        dir.insert(file_name, Entry::File(content.to_string()));
+        Ok(())
+    }
 }
